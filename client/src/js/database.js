220e3d7242543699e2dae1dccd8bd9d51c72f0dd
shru <shruthishekar@gmail.com>
2023-07-10 22:13:30 -0600

    Modified client and server
    -  dist folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>  console.error('putDb not implemented');
+export const putDb = async (content) =>  
 {
   console.log('putDb to the database');
 
@@ -35,7 +35,7 @@ export const putDb = async (content) =>  console.error('putDb not implemented');
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => 
 {
   console.log('GET all from the database');
 
@@ -54,7 +54,7 @@ export const getDb = async () => console.error('getDb not implemented');
     // Get confirmation of the request.
     const result = await request;
     console.log('result.value', result);
-    return result;
+    //return result;
   };
 // Start the database.
 initdb();
